867972747652fdb13452404327ba6241a2a51eed
DandanMF <danielfrancesconi91@gmail.com>
2024-11-02 10:05:48 -0300

    desafio do heroi e xp com entrada do usuario e analise de ranking

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-console.log("agora vai!!!!")
+const readline = require('readline');
 
-console.log("toma")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let nome;
+let xp;
+
+rl.question('Digite seu nome: ', (respostaNome) => {
+    nome = respostaNome;
+
+    rl.question('Digite sua quantidade de experiência: ', (xp) => {
+
+        if (xp <= 1000) {
+            console.log('Ferro!');
+        } else if (xp >= 1001 && xp <= 2000) {
+            console.log('Bronze!');
+        } else if (xp >= 2001 && xp <= 5000) {
+            console.log('Prata!');
+        } else if (xp >= 5001 && xp <= 7000) {
+            console.log('Ouro!');
+        } else if (xp >= 7001 && xp <= 8000) {
+            console.log('Platina!');
+        } else if (xp >= 8001 && xp <= 9000) {
+            console.log('Ascendente!');
+        } else if (xp >= 9001 && xp <= 10000) {
+            console.log('Imortal!');
+        } else {
+            console.log('Radiante!');
+        }
+
+        console.log('O Heroi de nome ' + '' + nome + '' + ' esta no nivel ' + '' + xp);
+        rl.close();
+    });
+});
